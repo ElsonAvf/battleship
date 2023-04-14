0187fbd94bf0604e6054542b98ad42d12d2cec40
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-04-14 19:03:48 -0300

    Create Ship class && Ship test

---
+++ src/utils/Ship/Ship.test.js
@@ -0,0 +1,13 @@
+import Ship from './Ship.js'
+
+test('Sunk if hits === length', () => {
+    const ship = new Ship(2)
+    ship.hit()
+    ship.hit()
+    expect(ship.isSunk()).toBe(true)
+})
+test('Hits === 1', () => {
+    const ship = new Ship(2)
+    ship.hit()
+    expect(ship.hits).toBe(1)
+})
